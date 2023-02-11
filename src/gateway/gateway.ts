4b7313d933c88626e3d43deaa984ba3bd94f0dcb
Jeromekene <mckenzieelegeonye@gmail.com>
2023-02-11 19:13:03 +0100

    first commit

---
+++ src/gateway/gateway.ts
@@ -0,0 +1,30 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets';
+import { Server } from 'socket.io'
+
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id)
+      console.log('connected')
+    })
+  }
+
+  @WebSocketServer()
+  server: Server
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    this.server.emit("onMessage", {
+      msg: "New message",
+      content: body
+    })
+  }
+}
+
